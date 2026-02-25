e8e81faeaf1a95e4fe2639c213877aecfb77f0d0
feewet <feewet@gmail.com>
2026-02-25 16:34:12 -0500

    remove dependencies

--- lib/vault-v2/src/imports/AdaptiveCurveIrmImport.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-// Copyright (c) 2025 Morpho Association
-pragma solidity 0.8.19;
-// Force foundry to compile the Adaptive Curve Irm without importing it in the tests.
-
-import {AdaptiveCurveIrm} from "../../lib/morpho-blue-irm/src/adaptive-curve-irm/AdaptiveCurveIrm.sol";
--- lib/vault-v2/src/interfaces/IAdapterRegistry.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-// Copyright (c) 2025 Morpho Association
-pragma solidity >=0.5.0;
-
-interface IAdapterRegistry {
-    function isInRegistry(address account) external view returns (bool);
-}
