6dbc58a9d8ac8b17cd711341695dd78cc6b78f48
JulissaDantes <julissadcj@gmail.com>
2026-08-31 15:53:58 -0400

    for arb

--- test/fork/ccip/CCIPForkBase.sol
+++ test/fork/ccip/CCIPForkBase.sol
@@ -275,7 +275,7 @@ abstract contract CCIPForkBase is ForkBaseTest {
         string memory DESTINATION_RPC_URL = vm.rpcUrl("dstSepolia");
 
         sourceFork = vm.createFork(SOURCE_RPC_URL, 11590237);
-        destinationFork = vm.createFork(DESTINATION_RPC_URL);
+        destinationFork = vm.createFork(DESTINATION_RPC_URL, 302799693);
 
         ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
         vm.makePersistent(address(ccipLocalSimulatorFork));
